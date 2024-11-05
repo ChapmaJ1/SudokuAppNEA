8c6c4c84d25e8dd43cd38ef774ddce45445ca549
James Chapman <jchapman1000@hotmail.com>
2024-11-05 11:31:27 +0000

    Add project files.

---
+++ SudokuAppNEA/SudokuAppNEA/Models/User.cs
@@ -0,0 +1,8 @@
+﻿namespace SudokuAppNEA.Models
+{
+    public class User
+    {
+        public required string Username { get; set; }
+        public required string Password { get; set; }
+    }
+}
