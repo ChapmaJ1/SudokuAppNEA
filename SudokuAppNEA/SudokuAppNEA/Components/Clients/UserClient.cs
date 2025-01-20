4a0234dbf073441c5cba6e33f2802d1a32134865
James Chapman <jchapman1000@hotmail.com>
2025-01-20 20:54:00 +0000

    css + html fixed for all pages

--- SudokuAppNEA/SudokuAppNEA/Components/Clients/UserClient.cs
+++ SudokuAppNEA/SudokuAppNEA/Components/Clients/UserClient.cs
@@ -1,5 +1,6 @@
 ﻿using CommonLibrary;
 using SQLDatabase;
+using Sudoku_Solver_NEA;
 using SudokuAppNEA.Components.Models;
 
 namespace SudokuAppNEA.Components.Clients
@@ -11,5 +12,7 @@ namespace SudokuAppNEA.Components.Clients
         public string? MistakeDetection { get; set; }
         public string? SaveScores { get; set; }
         public DatabaseEntry? Entry { get; set; }
+
+        public List<Board>? FetchedBoards { get; set; }
     }
 }
